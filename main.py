8d58c9eab69eefbc525225533926094007cab069
drpesbot <dr.gym9999@gmail.com>
2025-06-29 02:46:27 +0300

    إصلاح main.py وتحديث المكتبات

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from app import app, socketio
 import os
 
-if _name_ == '_main_':
-    port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    socketio.run(app, port=5000)
