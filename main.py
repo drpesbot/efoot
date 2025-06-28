1dbfb8532ee62e40f23192bbca92e57c96585a12
drpesbot <dr.gym9999@gmail.com>
2025-06-29 01:21:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from app import app, socketio
+import os
 
-if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+if _name_ == '_main_':
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
