e4ea5f838d43bdc2e85b1263338042435c840d6c
drpesbot <dr.gym9999@gmail.com>
2025-06-29 03:17:24 +0300

    Fix port binding for Render deployment

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from app import app, socketio
 import os
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, port=port, allow_unsafe_werkzeug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render يحدد البورت من هنا
+    socketio.run(app, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
